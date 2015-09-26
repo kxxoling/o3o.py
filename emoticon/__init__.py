2029bdc3a87523bfb401cfa08faa5517d7fb48e6
kxxoling <kxxoling@gmail.com>
2015-09-26 21:11:26 +0800

    Add function to find add .yaml files in module directory

--- emoticon/__init__.py
+++ emoticon/__init__.py
@@ -1,2 +1,13 @@
 __version__ = '0.0.5'
 
+
+def find_emoticon_data_files():
+    import os
+    import glob
+    abs_file_dir = os.path.abspath(__file__)
+    base_dir = os.path.dirname(abs_file_dir)
+    yaml_files = glob.glob(base_dir + '/*.yaml')
+    return yaml_files
+
+EMOTION_DATA_FILES = find_emoticon_data_files()
+
